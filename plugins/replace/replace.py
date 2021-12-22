c4f419077e8ebab9f2a15ca11eacf571a75f4187
Dan <14043624+delivrance@users.noreply.github.com>
2021-12-22 16:29:20 +0100

    Update copyright year

--- plugins/replace/replace.py
+++ plugins/replace/replace.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2018 BrightSide <https://github.com/bright5ide>
+# Copyright (c) 2018-present BrightSide <https://github.com/bright5ide>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
